40470bf62e3faf8f01178ff8881e24cf675eef83
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2021-04-05 17:03:35 +0200

    Update AbsentieDoorvoerenController.java
    Textfield gefixed.

--- SoftwareDevProject/src/main/java/UI/AbsentieDoorvoerenController.java
+++ SoftwareDevProject/src/main/java/UI/AbsentieDoorvoerenController.java
@@ -1,11 +1,12 @@
 package UI;
 
-import java.awt.TextField;
+
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
@@ -13,8 +14,8 @@ public class AbsentieDoorvoerenController {
 	public Label ingelogdPersoon;
 	public DatePicker beginDatum;
 	public DatePicker eindDatum;
-//	public TextField beginTijd;
-//	public TextField eindTijd;
+	public TextField beginTijd;
+	public TextField eindTijd;
 	
     public void initialize() {
     }
