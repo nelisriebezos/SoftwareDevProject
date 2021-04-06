b8b7a55ce60b4c73da436ed642a02fe6839c46ac
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2021-04-06 15:13:38 +0200

    Home2 aangepast

--- SoftwareDevProject/src/main/java/UI/Home2Controller.java
+++ SoftwareDevProject/src/main/java/UI/Home2Controller.java
@@ -1,17 +1,32 @@
 package UI;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
+import controllers.Objecten;
+import domeinKlassen.Klas;
+import domeinKlassen.Les;
+import domeinKlassen.Vak;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Home2Controller {
 	
+	public DatePicker overzichtDatePicker;
+	public ListView<Les> vakkenListView;
+	
 	public void initialize() {
+		overzichtDatePicker.setValue(LocalDate.now());
+        toonLessen();
 	}
 
 	public void AbsentieBekijken(ActionEvent actionEvent) {
@@ -28,4 +43,30 @@ public class Home2Controller {
 		newStage.initModality(Modality.APPLICATION_MODAL);
 		newStage.showAndWait();
 	}
+	
+	public void toonLessen() {
+		List<Vak> vakkenLijst = Objecten.getIngelogdDocent().getVakkenLijst();
+			for(Vak vak: vakkenLijst) {
+				List<Les> lessenLijst = vak.getLessenLijst();
+				for(Les les: lessenLijst) {
+					if(overzichtDatePicker.getValue().equals(les.getBeginTijd())) {
+						ObservableList<Les> lessen = FXCollections.observableArrayList();
+						lessen.add(les);
+						vakkenListView.setItems(FXCollections.observableArrayList(lessen));
+					}
+				}
+			}
+			
+		
+	}
+	
+	public void toonVorigeDag(ActionEvent actionEvent) {
+        LocalDate dagEerder = overzichtDatePicker.getValue().minusDays(1);
+        overzichtDatePicker.setValue(dagEerder);
+    }
+
+    public void toonVolgendeDag(ActionEvent actionEvent) {
+        LocalDate dagLater = overzichtDatePicker.getValue().plusDays(1);
+        overzichtDatePicker.setValue(dagLater);
+    }
 }
